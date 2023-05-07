7a0b5b2be82ad58ee3e32589f1bb90e17a2ca43f
Pierre-Antoine <patremblay22@gmail.com>
2023-05-07 16:46:13 +0200

    Initial commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,10 @@
+/**
+ * Import dependencies from node_modules
+ */
+// import * as bootstrap from 'bootstrap'
+
+/**
+ * Write any other JavaScript below
+ */
+import "bootstrap";
+import "../styles/main.scss";
